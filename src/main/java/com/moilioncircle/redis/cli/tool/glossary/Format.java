22f8c1bfe9a9edb799746d17437f4edc8ed06eb4
chenby <chenby@nextop.cn>
2018-07-09 21:58:36 +0800

    init

--- src/main/java/com/moilioncircle/redis/cli/tool/glossary/Format.java
+++ src/main/java/com/moilioncircle/redis/cli/tool/glossary/Format.java
@@ -1,6 +1,7 @@
 package com.moilioncircle.redis.cli.tool.glossary;
 
 import com.moilioncircle.redis.cli.tool.conf.Configure;
+import com.moilioncircle.redis.cli.tool.ext.rct.DiffRdbVisitor;
 import com.moilioncircle.redis.cli.tool.ext.rct.DumpRdbVisitor;
 import com.moilioncircle.redis.cli.tool.ext.rct.JsonRdbVisitor;
 import com.moilioncircle.redis.cli.tool.ext.rct.KeyRdbVisitor;
@@ -18,21 +19,22 @@ import java.util.List;
 public enum Format {
     KEY("key"),
     MEM("mem"),
+    DIFF("diff"),
     DUMP("dump"),
     JSON("json"),
     RESP("resp"),
     KEYVAL("keyval");
-
+    
     private String value;
-
+    
     Format(String value) {
         this.value = value;
     }
-
+    
     public String getValue() {
         return this.value;
     }
-
+    
     public static Format parse(String format) {
         switch (format) {
             case "key":
@@ -51,15 +53,19 @@ public enum Format {
                 throw new AssertionError("Unsupported format '" + format + "'");
         }
     }
-
+    
     public void dress(Replicator r, Configure conf, File output, List<Long> db, List<String> regexs, Long largest, Long bytes, List<DataType> types, Escape escape) {
         switch (this) {
+            case DIFF:
+                r.setRdbVisitor(new DiffRdbVisitor(r, conf, output, db, regexs, types));
+                break;
             case KEY:
                 r.setRdbVisitor(new KeyRdbVisitor(r, conf, output, db, regexs, types, escape));
                 break;
             case JSON:
                 r.setRdbVisitor(new JsonRdbVisitor(r, conf, output, db, regexs, types, escape));
                 break;
+            
             case DUMP:
                 r.setRdbVisitor(new DumpRdbVisitor(r, conf, output, db, regexs, types, escape));
                 break;
