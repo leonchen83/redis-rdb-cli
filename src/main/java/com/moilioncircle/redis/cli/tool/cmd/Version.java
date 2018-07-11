06b269f3980459fdd35da10a07dafa466d3b6267
chenby@nextop.cn <chenby1.2.3>
2018-07-11 10:54:12 +0800

    rct

--- src/main/java/com/moilioncircle/redis/cli/tool/cmd/Version.java
+++ src/main/java/com/moilioncircle/redis/cli/tool/cmd/Version.java
@@ -4,5 +4,5 @@ package com.moilioncircle.redis.cli.tool.cmd;
  * @author Baoyi Chen
  */
 public class Version {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.1.1";
 }
