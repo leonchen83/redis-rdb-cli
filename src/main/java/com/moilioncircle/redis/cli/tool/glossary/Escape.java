7d7c4b3252cb6343216aab6c9e2c754603659a38
chenby <chenby@nextop.cn>
2018-07-11 23:45:12 +0800

    init

--- src/main/java/com/moilioncircle/redis/cli/tool/glossary/Escape.java
+++ src/main/java/com/moilioncircle/redis/cli/tool/glossary/Escape.java
@@ -13,8 +13,6 @@ public enum Escape {
     RAW("raw"),
     REDIS("redis");
     
-    private static final char[] NUMERALS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-    
     private String value;
     
     Escape(String value) {
