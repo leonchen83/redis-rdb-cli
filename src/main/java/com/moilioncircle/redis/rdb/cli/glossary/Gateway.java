7d87ee025af1370487c13a04edbbfb7274b7b54d
chenby <chenby@nextop.cn>
2018-08-04 11:13:08 +0800

    metric

--- src/main/java/com/moilioncircle/redis/rdb/cli/glossary/Gateway.java
+++ src/main/java/com/moilioncircle/redis/rdb/cli/glossary/Gateway.java
@@ -6,6 +6,7 @@ package com.moilioncircle.redis.rdb.cli.glossary;
 public enum Gateway {
     
     LOG("log"),
+    NONE("none"),
     PROMETHEUS("prometheus");
     
     private String value;
@@ -20,6 +21,7 @@ public enum Gateway {
     
     public static Gateway parse(String value) {
         if (value.equals("log")) return LOG;
+        else if (value.equals("none")) return NONE;
         else if (value.equals("prometheus")) return PROMETHEUS;
         else throw new UnsupportedOperationException(value);
     }
--- src/main/java/com/moilioncircle/redis/rdb/cli/metric/MetricReporterFactory.java
+++ src/main/java/com/moilioncircle/redis/rdb/cli/metric/MetricReporterFactory.java
@@ -1,6 +1,7 @@
 package com.moilioncircle.redis.rdb.cli.metric;
 
 import com.moilioncircle.redis.rdb.cli.conf.Configure;
+import com.moilioncircle.redis.rdb.cli.metric.none.NoneReporter;
 import com.moilioncircle.redis.rdb.cli.metric.prometheus.PrometheusReporter;
 import com.moilioncircle.redis.rdb.cli.metric.prometheus.PrometheusSender;
 import com.moilioncircle.redis.rdb.cli.metric.prometheus.PrometheusUriSender;
@@ -14,11 +15,13 @@ import org.slf4j.LoggerFactory;
  * @author Baoyi Chen
  */
 public class MetricReporterFactory {
-
+    
     private static final Logger METRIC_LOGGER = LoggerFactory.getLogger("METRIC_LOGGER");
     
     public static ScheduledReporter create(Configure configure, MetricRegistry registry, String job) {
         switch (configure.getMetricGateway()) {
+            case NONE:
+                return NoneReporter.forRegistry(registry).build();
             case LOG:
                 return Slf4jReporter.forRegistry(registry).outputTo(METRIC_LOGGER).build();
             case PROMETHEUS:
